3135b15f54a1bce42b0f5d1b2ea3854d84a4e1bc
Zane Wang <zane@tarock.me>
2022-10-01 22:20:34 -0700

    Optimize card and answer CSS

--- react-app/src/component/Answer.js
+++ react-app/src/component/Answer.js
@@ -8,20 +8,10 @@ import btn_4 from '../assets/btn_4.svg';
 const Answer = ({ count, onClick }) => {
     return (
         <div className="answer-container">
-            <ul>
-                <li>
-                    <img src={btn_1} alt="btn_1"/>
-                </li>
-                <li>
-                    <img src={btn_2} alt="btn_2"/>
-                </li>
-                <li>
-                    <img src={btn_3} alt="btn_3"/>
-                </li>
-                <li>
-                    <img src={btn_4} alt="btn_4"/>
-                </li>
-            </ul>
+            <img src={btn_1} alt="btn_1"/>
+            <img src={btn_2} alt="btn_2"/>
+            <img src={btn_3} alt="btn_3"/>
+            <img src={btn_4} alt="btn_4"/>
         </div>
     );
 }

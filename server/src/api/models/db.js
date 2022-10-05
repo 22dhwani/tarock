480d6cd43fcdce8346eb94c9ace081c787c37ded
Zane Wang <zane@tarock.me>
2022-10-04 23:30:54 -0700

    Update db user and password

--- server/src/api/models/db.js
+++ server/src/api/models/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const con = mysql.createConnection({
   host: "34.173.211.162",
-  user: "zane",
-  password: "wz86135573",
+  user: "testuser",
+  password: "password",
   database: "tarock"
 });
 

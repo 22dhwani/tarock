971e0c8bb33925705f5ea1be709fdcaf973cc1a7
Raffid Ahmad <raffidahmad@gmail.com>
2022-12-05 04:56:51 +0500

    feed links + feed card fix

--- frontend/src/components/Feed/FeedCard.jsx
+++ frontend/src/components/Feed/FeedCard.jsx
@@ -1,29 +1,25 @@
 function FeedCard(props) {
-    const { image, title, author, time }=props;
+    const { image, title, author, time } = props;
     return (
-        <div className="d-flex flex-column mx-auto mb-2" style={{ width: 'fit-content', cursor: 'pointer' }}>
-            <div className='mb-3' >
-                <div  width='450px'></div>
-                <img src={image} alt="image"  style={{width: '100%',maxWidth: '100%', objectFit: 'cover'}}/>
+        <div className="d-flex flex-column mb-2 mt-3 mx-auto" style={{ width: 'fit-content', cursor: 'pointer' }}>
+            <img src={image} alt="image" style={{ width: '350px', objectFit: 'cover', height: '200px' }} className='rounded-3' />
+                <h5 style={{
+                    fontWeight: '700',
+                    color: '#49304D',
+                    lineHeight: '17px',
+                    textAlign: 'left',
+                    fontSize: '14px',
+                    marginTop: '10px',
+                    width: '350px'
+                }}>
+                    {title}
+                </h5>
+                <div className='d-flex justify-content-start'>
+                    <p className="card-text text-left" >By {author}</p>
+                    <p className="card-text" style={{ color: '#B6ACB8', marginLeft: '5em' }} >{time}h ago</p>
+                </div>
             </div>
-            <div className='mx-4'>
-            <h5 
-            style={{
-                fontFamily:'Montserrat',
-                fontWeight:'700',
-                color:'#49304D',
-                lineHeight:'17px',
-                textAlign:'left',
-                fontSize: '14px',
-            }}>{title}</h5>
-            <div className='d-flex justify-content-start'>
-            <p className="card-text text-left" >By {author}</p>
-            <p className="card-text" style={{color:'#B6ACB8', marginLeft:'5em'}} >{time}h ago</p>
-            </div>
-            </div>
-            
-
-        </div>
+      
     )
 }
 

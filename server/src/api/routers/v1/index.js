9b4a9ce1d3691a3874b15ec78e3be4669bfc12d7
Asif Patel <asif987patel@gmail.com>
2022-12-07 11:49:05 +0530

    added: questions list API

--- server/src/api/routers/v1/index.js
+++ server/src/api/routers/v1/index.js
@@ -1,9 +1,13 @@
 import test from '../../controllers/v1/test.js';
+import question from '../../controllers/v1/question.js';
 import express from 'express';
 
 export default function(app) {
     const router = express.Router();
 
+
+    //QUESTIONS
+    router.get("/questions", question.getQuestion);
 
     router.post("/test", test.test);
 

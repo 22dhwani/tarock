bb686cf445e6159a2479a6a83e8fa130ddcf32bb
Yash Chauhan <yc9274@gmail.com>
2022-12-13 15:53:16 +0530

    Added: loading with tarock logo

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,6 +1,9 @@
+import logo from '../../assets/tarockLogo.svg';
+
 const Loading = () => {
     return (
-        <div className="d-flex align-items-center justify-content-center vh-100">
+        <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+            <img src={logo} alt="logo" height='23.83px' width='120px' className='mb-5 mx-auto' />
             <div className="spinner-border" role="status">
                 <span className="sr-only"></span>
             </div>

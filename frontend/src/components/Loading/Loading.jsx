ae6ae4b4d6a7a0f29edfe68020e3592efcd46f57
Yash Chauhan <yc9274@gmail.com>
2022-12-14 10:32:10 +0530

    revert: loading with tarock logo

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,9 +1,6 @@
-import logo from '../../assets/tarockLogo.svg';
-
 const Loading = () => {
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center vh-100">
-            <img src={logo} alt="logo" height='23.83px' width='120px' className='mb-5 mx-auto' />
+        <div className="d-flex align-items-center justify-content-center vh-100">
             <div className="spinner-border" role="status">
                 <span className="sr-only"></span>
             </div>

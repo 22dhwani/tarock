4d33577ff8446520617f139dbf508eb8dad6ab9b
Yash Chauhan <yc9274@gmail.com>
2022-12-16 11:18:33 +0530

    Fix: size of image in gencard

--- frontend/src/components/Cards/GenCard.jsx
+++ frontend/src/components/Cards/GenCard.jsx
@@ -19,22 +19,22 @@ export default function GenCard(props) {
             setter('#B561D6');
         }
     }
-    
+
     useEffect(() => {
         assignColor(props.userQuadra, setUserColor);
         assignColor(props.matchedQuadra, setMatchedColor);
-        if(props.cardType == 'match'){
-            if(props.userQuadra == 'Alpha' && props.matchedQuadra == 'Beta' || props.userQuadra == 'Beta' && props.matchedQuadra == 'Alpha'){
+        if (props.cardType == 'match') {
+            if (props.userQuadra == 'Alpha' && props.matchedQuadra == 'Beta' || props.userQuadra == 'Beta' && props.matchedQuadra == 'Alpha') {
                 setCenterColor('#C0B17B 23.96%');
-            }else if(props.userQuadra == 'Alpha' && props.matchedQuadra == 'Gamma' || props.userQuadra == 'Gamma' && props.matchedQuadra == 'Alpha'){
+            } else if (props.userQuadra == 'Alpha' && props.matchedQuadra == 'Gamma' || props.userQuadra == 'Gamma' && props.matchedQuadra == 'Alpha') {
                 setCenterColor('#C0B17B 47.92%'); //to be updated
-            }else if(props.userQuadra == 'Alpha' && props.matchedQuadra == 'Delta' || props.userQuadra == 'Delta' && props.matchedQuadra == 'Alpha'){
+            } else if (props.userQuadra == 'Alpha' && props.matchedQuadra == 'Delta' || props.userQuadra == 'Delta' && props.matchedQuadra == 'Alpha') {
                 setCenterColor('#5E6AA9 40.1%');
-            }else if(props.userQuadra == 'Beta' && props.matchedQuadra == 'Gamma' || props.userQuadra == 'Gamma' && props.matchedQuadra == 'Beta'){
+            } else if (props.userQuadra == 'Beta' && props.matchedQuadra == 'Gamma' || props.userQuadra == 'Gamma' && props.matchedQuadra == 'Beta') {
                 setCenterColor('#AFBE74 23.96');
-            }else if(props.userQuadra == 'Beta' && props.matchedQuadra == 'Delta' || props.userQuadra == 'Delta' && props.matchedQuadra == 'Beta'){
+            } else if (props.userQuadra == 'Beta' && props.matchedQuadra == 'Delta' || props.userQuadra == 'Delta' && props.matchedQuadra == 'Beta') {
                 setCenterColor('#BEA074 31.25%');
-            }else if(props.userQuadra == 'Gamma' && props.matchedQuadra == 'Delta' || props.userQuadra == 'Delta' && props.matchedQuadra == 'Gamma'){
+            } else if (props.userQuadra == 'Gamma' && props.matchedQuadra == 'Delta' || props.userQuadra == 'Delta' && props.matchedQuadra == 'Gamma') {
                 setCenterColor('#5EA99B 32.29%');
             }
         }
@@ -44,8 +44,8 @@ export default function GenCard(props) {
     return (
         <div>
             <div style={{
-                backgroundImage: props.cardType == 'match' ? 
-                `linear-gradient(180deg, ${userColor} 0%, ${centerColor}, ${matchedColor} 100%)` : `url(${cardPattern})`,
+                backgroundImage: props.cardType == 'match' ?
+                    `linear-gradient(180deg, ${userColor} 0%, ${centerColor}, ${matchedColor} 100%)` : `url(${cardPattern})`,
                 backgroundColor: props.cardType == 'match' ? '' : userColor,
                 backgroundRepeat: 'no-repeat',
                 width: '9rem',
@@ -57,20 +57,23 @@ export default function GenCard(props) {
                 justifyContent: 'center',
                 alignItems: 'center',
             }}>
-                {props.cardType !== 'match' ? <img className='rounded-circle'
-                    src={getAvatar(props.avatar_index)}
-                    alt="avatar"
-                    style={{ backgroundColor: '#FFFFFF' }}
-                />
+                {props.cardType !== 'match' ?
+                    <img className='rounded-circle'
+                        src={getAvatar(props.avatar_index)}
+                        alt="avatar"
+                        style={{ backgroundColor: '#FFFFFF' }}
+                        width='45px'
+                        height='45px'
+                    />
                     :
                     <div className='d-flex flex-column gap-5 align-items-center'>
-                        <img src={getAvatar(props.avatar_index)} className='rounded-circle' 
-                        style={{ backgroundColor: '#FFFFFF' }} 
-                        width='40px' height='40px'
+                        <img src={getAvatar(props.avatar_index)} className='rounded-circle'
+                            style={{ backgroundColor: '#FFFFFF' }}
+                            width='40px' height='40px'
                         />
-                        <img src={getAvatar(props.matchedUserAvartarIndex)} className='rounded-circle' 
-                        style={{ backgroundColor: '#FFFFFF' }}
-                        width='40px' height='40px' />
+                        <img src={getAvatar(props.matchedUserAvartarIndex)} className='rounded-circle'
+                            style={{ backgroundColor: '#FFFFFF' }}
+                            width='40px' height='40px' />
                     </div>
                 }
 
@@ -84,7 +87,7 @@ export default function GenCard(props) {
             }}>
                 {props.cardType == 'match' ? <p className='text-center'>
                     Match Card<br></br>
-                    <span style={{fontWeight:'400'}}>with {props.matchedUserName.split(' ')[0]}</span>
+                    <span style={{ fontWeight: '400' }}>with {props.matchedUserName.split(' ')[0]}</span>
                 </p>
                     :
                     'Tarock Card'}

7962b8d7446d671571f078008275b4a9902ffb0e
Asif Patel <asif987patel@gmail.com>
2022-12-23 12:19:00 +0530

    added: custome avatar thing

--- server/src/api/models/exploreComment.js
+++ server/src/api/models/exploreComment.js
@@ -9,7 +9,7 @@ const ExploreComment = function(exploreComment) {
 }
 
 async function getForExplore(exploreId,internal_user_id) {
-    const data = await sql.query("SELECT explore_comments.* , user.name,user.avatar_index, CASE WHEN EXISTS(select * from user_to_comment where `user_to_comment`.`internal_user_id` = ? AND `user_to_comment`.`explore_comment_id` = explore_comments.id) then 1 else 0 end as is_liked FROM explore_comments LEFT JOIN user ON explore_comments.internal_user_id = user.internal_user_id WHERE explore_id = ? ORDER BY created_at DESC;",[internal_user_id,exploreId]);
+    const data = await sql.query("SELECT explore_comments.* , user.name,user.avatar_index,user_avatars.face_index,user_avatars.hair_index,user_avatars.eyebrow_index,user_avatars.eye_index,user_avatars.nose_index,user_avatars.whiskers_index,user_avatars.beard_index,user_avatars.lips_index,user_avatars.ear_index,user_avatars.glasses_index, CASE WHEN EXISTS(select * from user_to_comment where `user_to_comment`.`internal_user_id` = ? AND `user_to_comment`.`explore_comment_id` = explore_comments.id) then 1 else 0 end as is_liked FROM explore_comments LEFT JOIN user ON explore_comments.internal_user_id = user.internal_user_id LEFT JOIN user_avatars ON user.internal_user_id = user_avatars.internal_user_id WHERE explore_id = ? ORDER BY created_at DESC;",[internal_user_id,exploreId]);
     return data[0];
 }
 

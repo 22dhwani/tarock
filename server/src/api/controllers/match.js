361c2deb69cf45542287e887a77d380438703cf0
Asif Patel <asif987patel@gmail.com>
2022-12-29 18:44:07 +0530

    added: notification for matched user

--- server/src/api/controllers/match.js
+++ server/src/api/controllers/match.js
@@ -1,4 +1,6 @@
 import Match from '../models/match.js';
+import notification from '../models/notification.js';
+import User from '../models/user.js';
 
 async function create(req, res) {
     if (req.body.origUserId === req.body.matchedUserId) {
@@ -9,7 +11,14 @@ async function create(req, res) {
             matchedUserId: req.body.matchedUserId,
         });
 
+       
+
         try {
+            let user = User.queryReal(req.body.origUserId)
+            user = user[0]
+            if(user.is_notification_on && user.is_match_card_notification_on ){
+                await notification.sendToUserId(user.internal_user_id,req.body.matchedUserId,'MATCH_CARD','You have a new matched card with Kevin Wrangler.',null,0,null)
+            }
             const data = await Match.create(match);
             res.send(data);
         } catch (error) {

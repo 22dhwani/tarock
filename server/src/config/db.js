ce9a960b52eae12c25973c80c3e9ee21e0833adb
dhwani22 <dhwanisheth2202@gmail.com>
2023-06-07 10:15:56 +0530

    changes

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const con = mysql.createPool({
-  host: process.env['DB_HOST'],
-  user: process.env['DB_USER'],
-  password: process.env['DB_PWD'],
-  database: 'tarock'
+  host: process.env["DB_HOST"],
+  user: process.env["DB_USER"],
+  password: process.env["DB_PWD"],
+  database: "tarock-version2-test",
 });
 
 export default con;
